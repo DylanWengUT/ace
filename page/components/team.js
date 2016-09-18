35533df503d44cc47147b212151d2a21369c7b53
rainshen49 <slk49@live.cn>
2016-09-18 15:30:17 -0400

    finish sample for the about us section

---
+++ page/components/team.js
@@ -0,0 +1,18 @@
+(function loadTeam() {
+    const sample = $('.ppl');
+    const tem = sample.parentNode;
+    AjaxPromise('get', 'data/team.json', 'json').then(response => {
+        response.forEach(({ pic, name, title, intro, contact }) => {
+            const newNode = sample.cloneNode(sample, true);
+            const [img, cont] = newNode.children;
+            const [h2, h3, p, div] = cont.children;
+            img.setAttribute('src', pic);
+            h2.textContent = name;
+            h3.textContent = title;
+            p.innerHTML = intro;
+            div.innerHTML = contact;
+            tem.appendChild(newNode);
+        })
+        sample.style.display = 'none';
+    })
+})()
--- page/js/main.js
+++ page/js/main.js
@@ -1,5 +1,24 @@
-AjaxPromise('get', 'components/navbar.html', 'document').then(doc => {
-    const tem = $('template', doc);
-    const nav = document.importNode($('#navTop', tem.content),true);
-    document.body.appendChild(nav);
-})
+const nav = $('#navTop')
+AjaxPromise('get', '/data/menus.json', 'json')
+    .then(response => {
+        response.forEach(item => {
+            const ele = creatE('a', { class: 'menuItem' });
+            ele.textContent = item;
+            ele.setAttribute('href', '/#' + item)
+            nav.appendChild(ele);
+        })
+        loadNavigation();
+    })
+
+function loadNavigation() {
+    $('a[href="/#About Us"]').addEventListener('click', ev => {
+        if (window.location.href.endsWith('About Us')) {
+            $('main').innerHTML = '';
+        }
+        AjaxPromise('get', 'components/team.html', 'document').then(doc => {
+            const tem = $('template', doc);
+            const team = document.importNode(tem.content, true);
+            $('#container').appendChild(team);
+        })
+    })
+}
