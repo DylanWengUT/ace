69efc9d3461256bdc3dd12b6ad7bea4a76589c0a
rainshen49 <slk49@live.cn>
2016-09-18 16:05:02 -0400

    ok

--- serverSSL.js
+++ serverSSL.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
     console.log(req.method, req.url);
     next();
 })
-app.use(express.static(__dirname+"/page"));
+app.use(express.static(__dirname + "/docs"));
 app.get('/', function(req, res) {
     res.sendFile('index.html');
 });
