3572f7bdf9753bca88c7f9542e52659ab299ffc0
rainshen49 <slk49@live.cn>
2016-09-18 16:21:21 -0400

    change all

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const nav = $('#navTop')
+AjaxPromise('get', '/data/menus.json', 'json')
+    .then(response => {
+        response.forEach(item => {
+            const ele = creatE('a', { class: 'menuItem' });
+            ele.textContent = item;
+            ele.setAttribute('href', '/#' + item)
+            nav.appendChild(ele);
+        })
+        loadNavigation();
+    })
+
+function loadNavigation() {
+    $('a[href="/#About Us"]').addEventListener('click', ev => {
+        if (window.location.href.endsWith('About Us')) {
+            $('main').innerHTML = '';
+        }
+        AjaxPromise('get', 'components/team.html', 'document').then(doc => {
+            const tem = $('template', doc);
+            const team = document.importNode(tem.content, true);
+            $('#container').appendChild(team);
+        })
+    })
+}
