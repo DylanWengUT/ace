e4bff04ccb6121bb3b77c674d9c08c67db570635
rainshen49 <slk49@live.cn>
2016-09-24 02:05:15 -0400

    built main events. optimize main.js code

--- components/events.js
+++ components/events.js
@@ -2,16 +2,27 @@
     const sample = $('.event');
     const tem = sample.parentNode;
     AjaxPromise('get', 'data/events.json', 'json').then(response => {
-        response.forEach(({ event, pic, description, location, time }) => {
-            const newNode = sample.cloneNode(sample, true);
-            const [img, h2, p, info] = newNode.children;
-            const [loc, tim] = info.children;
+        let main = true;
+        response.forEach(({ event, pic, description, location, time, link }) => {
+
+            if (main) {
+                main = false
+                var newNode = $('#mEvent')
+                var [img, banner] = newNode.children;
+                var [h, p, a, info] = banner.children;
+                a.setAttribute('href', link)
+            } else {
+                var newNode = sample.cloneNode(sample, true);
+                var [img, h, p, info] = newNode.children;
+                var [loc, tim] = info.children;
+                loc.textContent = location
+                tim.textContent = time;
+            }
             img.setAttribute('src', pic);
-            h2.textContent = event;
+            h.textContent = event;
             p.textContent = description;
-            loc.textContent = location
-            tim.textContent = time;
             tem.appendChild(newNode);
+
         })
         sample.style.display = 'none';
     })
--- js/main.js
+++ js/main.js
@@ -4,62 +4,35 @@ function loadNavigation() {
     const nav = $('#navTop')
     AjaxPromise('get', 'data/menus.json', 'json')
         .then(response => {
-            response.forEach(item => {
+            for (let item in response) {
                 const ele = creatE('a', { class: 'menuItem' });
                 ele.textContent = item;
                 ele.setAttribute('href', '#' + item)
                 nav.appendChild(ele);
-            })
-            navigate();
-        })
+                bindComponentToLink(item, response[item]);
+            }
+            return Object.keys(response)
+        }).then(links => navigate(links))
 }
 
-function navigate() {
-    $('a[href="#About Us"]').addEventListener('click', ev => {
+function bindComponentToLink(link, filename) {
+    $(`a[href="#${link}"]`).addEventListener('click', ev => {
         if (mounted) $('#container').innerHTML = '';
-        AjaxPromise('get', 'components/team.html', 'document').then(doc => {
+        AjaxPromise('get', `components/${filename}`, 'document').then(doc => {
             const tem = $('template', doc);
-            const team = document.importNode(tem.content, true);
-            $('#container').appendChild(team);
+            const com = document.importNode(tem.content, true);
+            $('#container').appendChild(com);
             mounted = true;
         })
     })
-    $('a[href="#Events"]').addEventListener('click', ev => {
-        if (mounted) $('#container').innerHTML = '';
-        AjaxPromise('get', 'components/events.html', 'document').then(doc => {
-            const tem = $('template', doc);
-            const events = document.importNode(tem.content, true);
-            $('#container').appendChild(events);
-            mounted = true;
-        })
+}
+
+function navigate(links) {
+    links.forEach(link => {
+        if (window.location.href.endsWith(link)) {
+            $(`a[href="#${link}"]`).click();
+        }
     })
-    $('a[href="#Our Sponsors"]').addEventListener('click', ev => {
-        if (mounted) $('#container').innerHTML = '';
-        AjaxPromise('get', 'components/sponsors.html', 'document').then(doc => {
-            const tem = $('template', doc);
-            const sponsors = document.importNode(tem.content, true);
-            $('#container').appendChild(sponsors);
-            mounted = true;
-        })
-    })
-    $('a[href="#Contact Us"]').addEventListener('click', ev => {
-        if (mounted) $('#container').innerHTML = '';
-        AjaxPromise('get', 'components/contact.html', 'document').then(doc => {
-            const tem = $('template', doc);
-            const contact = document.importNode(tem.content, true);
-            $('#container').appendChild(contact);
-            mounted = true;
-        })
-    })
-    if (window.location.href.endsWith('About Us')) {
-        $('a[href="#About Us"]').click();
-    } else if (window.location.href.endsWith('Events')) {
-        $('a[href="#Events"]').click()
-    } else if (window.location.href.endsWith('Sponsors')) {
-        $('a[href="#Out Sponsors"]').click()
-    } else if (window.location.href.endsWith('Contact Us')) {
-        $('a[href="#Contact Us"]').click()
-    }
 }
 
 loadNavigation();
